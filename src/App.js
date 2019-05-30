827195c5bf801d9c301a3ee31e6733fe87e0b88b
stevoST <stefan.starosta@gmail.com>
2019-05-31 00:35:37 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
         let articles = this.state.articles.map((article) => {
             return (
                 <div class="jumbotron">
-                    <h4>{article.title}</h4>
+                    <a href={article.link}><h4>{article.title}</h4></a>
                     {article.description}
                 </div>
 
@@ -28,6 +28,16 @@ class App extends Component {
 
         return (
             <div className="App">
+
+                <nav class="navbar navbar-dark bg-dark">
+                    <a class="navbar-brand" href="#">RSS Reader</a>
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-item" href="#">Show All RSS</a></li>
+                    </ul>
+                </nav>
+
+
                 <div class="container">
                     <div class="row">
                         <div class="col"></div>
