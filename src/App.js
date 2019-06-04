70234022bfc8ef3e707c344f41c110c5f07d82e3
stevoST <stefan.starosta@gmail.com>
2019-06-04 23:48:59 +0200

    added navbar links. does not work yet

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Nav from './Nav';
 import Configuration from './Configuration';
 import Feeds from './Feeds';
-
+import Homepage from "./Homepage";
 
 
 function App() {
     return (
         <Router>
-        <div>
-            <Nav/>
-            <Route path="/configuration" component={Configuration} />
-            <Route path="/feeds" component={Feeds} />
-        </div>
+            <div>
+                <Nav/>
+                <switch>
+                    <Route path="/" exact component={Homepage}/>
+                    <Route path="/configuration" component={Configuration}/>
+                    <Route path="/feeds" component={Feeds}/>
+                </switch>
+            </div>
         </Router>
     )
 }
--- src/Nav.js
+++ src/Nav.js
@@ -1,17 +1,26 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+import Feeds from "./Feeds";
+import Configuration from "./Configuration";
+import Homepage from "./Homepage";
 
 
 function Nav() {
     return (
         <nav className="navbar navbar-dark bg-dark">
-            <a className="navbar-brand" href="#">RSS Reader</a>
+            {/*<a className="navbar-brand" href="#">RSS Reader</a>*/}
+            <Link to={Homepage}>RSS Reader </Link>
             <ul className="navbar-nav">
-                <li className="nav-item active">Feeds
-                    {/*<a className="nav-item" href="#">Show All RSS</a>*/}
-                </li>
-                <li className="nav-item">Configuration
-                    {/*<a className="nav-item" href="#">Show All RSS</a>*/}
-                </li>
+                <Link to={Feeds}>
+                    <li className="nav-item active">Feeds
+                        {/*<a className="nav-item" href="#">Show All RSS</a>*/}
+                    </li>
+                </Link>
+                <Link to={Configuration}>
+                    <li className="nav-item">Configuration
+                        {/*<a className="nav-item" href="#">Show All RSS</a>*/}
+                    </li>
+                </Link>
             </ul>
         </nav>
     )
