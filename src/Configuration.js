292a6fe5866f74000f66a5441e16aa8629bb5f68
stevoST <stefan.starosta@gmail.com>
2019-06-30 14:11:47 +0200

    added edit button. still some problems with feedlink. adding new feed still not fixed

--- src/Configuration.js
+++ src/Configuration.js
@@ -7,7 +7,14 @@ class Configuration extends Component{
     state = {
         configurations: [],
         addFeedModal: false,
+        editFeedModal: false,
         newFeedData: {
+            feedName: '',
+            feedLink: ''
+        },
+
+        editFeedData: {
+            id: '',
             feedName: '',
             feedLink: ''
         }
@@ -28,6 +35,12 @@ class Configuration extends Component{
         })
     }
 
+    toggleEditFeedModal(){
+        this.setState({
+            editFeedModal: ! this.state.editFeedModal
+        })
+    }
+
     addFeed(){
         axios.post('http://localhost:8080/configuration', this.state.newFeedData).then((response) => {
             let { configurations } = this.state;
@@ -37,8 +50,20 @@ class Configuration extends Component{
             this.setState({ configurations, addFeedModal: false, newConfigurationsData: {
                     feedName: '',
                     feedLink: ''
-                }})
+                }});
         });
+    }
+
+    editFeed(id, feedName, feedLink){
+        this.setState({
+            editFeedData: {id, feedName, feedLink},
+            editFeedModal: !this.state.editFeedModal
+        })
+
+    }
+
+    updateFeed(){
+
     }
 
     render()
@@ -52,7 +77,7 @@ class Configuration extends Component{
                 <td>{configuration.feedName}</td>
                 <td>{configuration.feedLink}</td>
                 <td>
-                    <button type="button" className="btn btn-success">Edit</button>
+                    <button type="button" className="btn btn-success" onClick={this.editFeed.bind(this, configuration.id, configuration.feedName, configuration.FeedLink)}>Edit</button>
                     <button type="button" className="btn btn-danger">Delete</button>
                 </td>
             </tr>
@@ -67,7 +92,7 @@ class Configuration extends Component{
 
                 <h1>Configuration</h1>
 
-                <Button color="primary" onClick={this.toggleAddFeedModal.bind(this)}>Add Feed</Button>
+                <Button className="my-3" color="primary" onClick={this.toggleAddFeedModal.bind(this)}>Add Feed</Button>
                 <Modal isOpen={this.state.addFeedModal} toggle={this.toggleAddFeedModal.bind(this)}>
                     <ModalHeader toggle={this.toggleAddFeedModal.bind(this)}>Add New Feed</ModalHeader>
                     <ModalBody>
@@ -95,6 +120,37 @@ class Configuration extends Component{
                     <ModalFooter>
                         <Button color="primary" onClick={this.addFeed.bind(this)}>Add Feed</Button>{' '}
                         <Button color="secondary" onClick={this.toggleAddFeedModal.bind(this)}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+
+
+                <Modal isOpen={this.state.editFeedModal} toggle={this.toggleEditFeedModal.bind(this)}>
+                    <ModalHeader toggle={this.toggleEditFeedModal.bind(this)}>Edit Feed</ModalHeader>
+                    <ModalBody>
+                        <FormGroup>
+                            <Label for="feedName">Feed Name</Label>
+                            <Input id="feedName" value={this.state.editFeedData.feedName} onChange={(e) => {
+                                let { editFeedData } = this.state;
+
+                                editFeedData.feedName = e.target.value;
+
+                                this.setState({editFeedData});
+                            }}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="feedLink">Feed Link</Label>
+                            <Input id="feedLink" value={this.state.editFeedData.feedLink} onChange={(e) => {
+                                let { editFeedData } = this.state;
+
+                                editFeedData.feedLink = e.target.value;
+
+                                this.setState({ editFeedData });
+                            }} />
+                        </FormGroup>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={this.updateFeed.bind(this)}>Update Feed</Button>{' '}
+                        <Button color="secondary" onClick={this.toggleEditFeedModal.bind(this)}>Cancel</Button>
                     </ModalFooter>
                 </Modal>
 
