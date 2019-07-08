fac7c1db416cc94a4c4d6ccb9de2b5b92b7d7194
stevoST <stefan.starosta@gmail.com>
2019-07-08 16:55:42 +0200

    changed navbar style

--- src/Nav.js
+++ src/Nav.js
@@ -6,17 +6,16 @@ import {Link} from 'react-router-dom'
 function Nav() {
     return (
         <nav className="navbar navbar-dark bg-dark">
-            {/*<a className="navbar-brand" href="#">RSS Reader</a>*/}
-            <Link to="/">RSS Reader </Link>
-            <ul className="navbar-nav">
-                <Link to="/feeds">
-                    <li className="nav-item active">Feeds
-                    </li>
-                </Link>
-                <Link to="/configuration">
-                    <li className="nav-item">Configuration
-                    </li>
-                </Link>
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <Link className="nav-link active" to="/">RSS Reader </Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link"  to="/feeds">Feeds</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/configuration">Configuration</Link>
+                </li>
             </ul>
         </nav>
     )
