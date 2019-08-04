f211854a236e955ebd4bef8242c1ff1eb5070e19
stevoST <stefan.starosta@gmail.com>
2019-08-04 23:56:56 +0200

    detail page is now parsing data

--- src/ConfigurationDetail.js
+++ src/ConfigurationDetail.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
+import axios from "axios/index";
 
 
 class ConfigurationDetail extends Component {
+
+    state = {
+        configuration: []
+    }
+
+    componentWillMount() {
+        axios.get('http://localhost:8080/configuration/' + this.props.match.params.id).then((response) => {
+            this.setState({
+                configuration: response.data
+            })
+        });
+
+    }
     render() {
         return (
             <div>
                 <h1>Detail Page</h1>
+                {this.state.configuration.feedName}
             </div>
         )
     }
