ce7bef398c3da2491490f11d5d98f49ed5c40366
stevoST <stefan.starosta@gmail.com>
2019-08-27 22:48:37 +0200

    handle change now works. text is editable

--- src/ConfigurationDetail.js
+++ src/ConfigurationDetail.js
@@ -5,53 +5,62 @@ import axios from "axios/index";
 class ConfigurationDetail extends Component {
 
     state = {
-        configuration: []
+        id: null,
+        feedName: '',
+        feedLink: '',
+        feedDateFormat: ''
     }
 
-    handleChange(event){
+    handleFeedNameChange = (event) => {
         this.setState({feedName: event.target.value});
-        console.log(this.state);
-        console.log(event);
     }
 
-    componentWillMount() {
+    handleFeedLinkChange = (event) => {
+        this.setState({feedLink: event.target.value});
+    }
+
+    handleFeedDateFormat = (event) => {
+        this.setState({feedDateFormat: event.target.value});
+    }
+
+    componentDidMount() {
         axios.get('http://localhost:8080/configuration/' + this.props.match.params.id).then((response) => {
-            console.log(response.data);
-            this.setState({
-                configuration: response.data
-            })
+               const { id, feedName , feedLink, feedDateFormat } = response.data;
+               this.setState({
+                   id, feedName , feedLink, feedDateFormat
+               })
         });
 
     }
     render() {
-            const { configuration } = this.state;
+        const { id, feedName , feedLink, feedDateFormat } = this.state;
         return (
             <div className="container mt-5">
 
                 <h1>Detail Page</h1>
-                {configuration.id} <br />
+                {id} <br />
                 <div className="input-group mb-3">
                     <div className="input-group-prepend">
                         <span className="input-group-text" id="inputGroup-sizing-default">Feed Name</span>
                     </div>
-                    <input type="text" value={configuration.feedName} className="form-control" aria-label="Sizing example input"
-                           aria-describedby="inputGroup-sizing-default"  onChange={this.handleChange.bind(this)}/>
+                    <input type="text" value={feedName} className="form-control" aria-label="Sizing example input"
+                           aria-describedby="inputGroup-sizing-default" onChange={this.handleFeedNameChange} />
                 </div>
 
                 <div className="input-group mb-3">
                     <div className="input-group-prepend">
                         <span className="input-group-text" id="inputGroup-sizing-default">Feed Link</span>
                     </div>
-                    <input type="text" value={configuration.feedLink} className="form-control" aria-label="Sizing example input"
-                           aria-describedby="inputGroup-sizing-default" />
+                    <input type="text" value={feedLink} className="form-control" aria-label="Sizing example input"
+                           aria-describedby="inputGroup-sizing-default" onChange={this.handleFeedLinkChange}/>
                 </div>
 
                 <div className="input-group mb-3">
                     <div className="input-group-prepend">
                         <span className="input-group-text" id="inputGroup-sizing-default">Date Format</span>
                     </div>
-                    <input type="text" value={configuration.feedDateFormat} className="form-control" aria-label="Sizing example input"
-                           aria-describedby="inputGroup-sizing-default" />
+                    <input type="text" value={feedDateFormat} className="form-control" aria-label="Sizing example input"
+                           aria-describedby="inputGroup-sizing-default" onChange={this.handleFeedDateFormat}/>
                 </div>
 
             </div>
