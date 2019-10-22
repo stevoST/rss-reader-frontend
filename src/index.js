98d98d68a082a6c7ecd252f721760eac4475197f
DOMAIN\starosta <starosta@cngroup.dk>
2019-10-22 16:35:47 +0200

    added another link

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 
 //folder structure
 //https://codeburst.io/atomic-design-with-react-e7aea8152957
+//https://medium.com/backticks-tildes/visually-breaking-down-ui-components-using-atomic-design-and-building-with-react-part-2-20eb8aabab4b
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
